f234fc9a1f56d4991d1c45e0f7327d9ff756f84c
soulayman <soulaymanjaa09@gmail.com>
2025-12-08 14:51:58 +0100

    created truck routes

---
+++ back-end/routes/truckRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router=express.Router();
+const truckController=require('../controllers/truckController')
+const Authenticated =require('../middlewares/Authenticated');
+const AuthorizedRole =require('../middlewares/AuthorizedRole');
+
+
+
+router.post('/',Authenticated,AuthorizedRole('Admin'),truckController.create);
+router.get('/',Authenticated,AuthorizedRole('Admin'),truckController.getAll);
+router.get('/:id',Authenticated,AuthorizedRole('Admin'),truckController.getTruckById);
+router.put('/:id',Authenticated,AuthorizedRole('Admin'),truckController.update);
+router.delete("/:id", Authenticated, AuthorizedRole("Admin"), truckController.delete);
