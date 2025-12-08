29e2cb23c25d2fb49e515a73a010b7222ff3b7f8
soulayman <soulaymanjaa09@gmail.com>
2025-12-08 14:55:11 +0100

    created trailer routes

---
+++ back-end/routes/trailerRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const trailerController = require("../controllers/trailerController");
+
+const Authenticated =require('../middlewares/Authenticated');
+const AuthorizedRole =require('../middlewares/AuthorizedRole');
+
+router.post("/", Authenticated, AuthorizedRole("Admin"), trailerController.create);
+router.get("/", Authenticated, AuthorizedRole("Admin"), trailerController.getAll);
+router.get("/:id", Authenticated, AuthorizedRole("Admin"), trailerController.getById);
+router.put("/:id", Authenticated, AuthorizedRole("Admin"), trailerController.update);
+router.delete("/:id", Authenticated, AuthorizedRole("Admin"), trailerController.delete);
+
+module.exports = router;
