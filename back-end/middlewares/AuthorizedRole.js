d22f7cd094cfe84dc97bbab101ed5f02b8ebd223
soulayman <soulaymanjaa09@gmail.com>
2025-12-09 10:04:41 +0100

    TP-16 #DONE created route protection by role middleware TP-1 #IN-PROGRESS

---
+++ back-end/middlewares/AuthorizedRole.js
@@ -0,0 +1,19 @@
+
+
+
+
+export default function AuthorizedRole() {
+    
+  try {
+    if (!req.user) return res.status(401).json({ message: "Unauthorized" });
+
+    if (req.user.role !== role) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+}
