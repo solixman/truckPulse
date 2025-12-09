938f9e4149f1786e95065acfa4303cf21d5a9ab1
soulayman <soulaymanjaa09@gmail.com>
2025-12-09 09:57:54 +0100

    TP-15 #DONE made the auth middelware

--- back-end/middlewares/Authenticated.js
+++ back-end/middlewares/Authenticated.js
@@ -1,14 +1,43 @@
+export default async function isAuthenticated() {
+  try {
+    const authorization = req.headers["authorization"];
+    const token = authorization && authorization.split(" ")[1];
+    const refreshToken = req.cookies?.jwt;
 
+    if (!token) return res.status(401).json({ message: "No token provided" });
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        if (err.name === "TokenExpiredError" && refreshToken) {
+          try {
+            const user = jwt.verify(
+              refreshToken,
+              process.env.REFRESH_TOKEN_SECRET
+            );
+            const newAccessToken = jwt.sign(
+              { username: user.username, email: user.email },
+              process.env.ACCESS_TOKEN_SECRET,
+              { expiresIn: "1m" }
+            );
 
-export default async function isAuthenticated(){
-try {
-    
-
-} catch (error) {
+            res.setHeader("x-access-token", newAccessToken);
+            req.user = user;
+            return next();
+          } catch {
+            return res.status(403).json({ message: "Invalid refresh token" });
+          }
+        } else {
+          return res.status(403).json({ message: "Invalid token" });
+        }
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } catch (error) {
     console.log(error);
-    return redirect('/')
+    res
+      .status(500)
+      .json({ message: "Something went wrong, please try again later" });
+  }
 }
-
-
-} 
