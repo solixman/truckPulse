47c9ea1b895c592802e225eb2f01feb8d40087c9
soulayman <soulaymanjaa09@gmail.com>
2025-12-09 10:09:13 +0100

    TP-16 #DONE TP-1 #IN-PROGRESS testing jira

--- back-end/controllers/truckController.js
+++ back-end/controllers/truckController.js
@@ -32,3 +32,23 @@ try {
     res.status(400).json({ message: err.message });
 }
 }
+
+//tzqting
+
+export async function getAll(req,res){
+try {
+
+    const filters={
+    licensePlate:req.query.licensePlate,
+    model:req.query.model,
+    mileage:req.query.mileage,
+    status:req.query.status,
+    }
+
+   const trucks = await TruckService.getAll(filters,req.body)
+  return res.status(200).json({trucks});
+} catch (error) {
+    console.log(error);
+    res.status(400).json({ message: err.message });
+}
+}
