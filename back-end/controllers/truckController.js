2c7fbe98551754edbcaf773fe831621782d4a16c
soulayman <soulaymanjaa09@gmail.com>
2025-12-09 09:16:13 +0100

    tp-10 #done created API routes

--- back-end/controllers/truckController.js
+++ back-end/controllers/truckController.js
@@ -1,4 +1,4 @@
-
+const TruckService = require('../services/truckService')
 
 
 export async function create(req,res) {
@@ -12,4 +12,23 @@ export async function create(req,res) {
     console.log(error);
     res.status(400).json({ message: err.message });
   }
+}
+
+
+export async function getAll(req,res){
+try {
+
+    const filters={
+    licensePlate:req.query.licensePlate,
+    model:req.query.model,
+    mileage:req.query.mileage,
+    status:req.query.status,
+    }
+
+   const trucks = await TruckService.getAll(filters,req.body)
+  return res.status(200).json({trucks});
+} catch (error) {
+    console.log(error);
+    res.status(400).json({ message: err.message });
+}
 }
