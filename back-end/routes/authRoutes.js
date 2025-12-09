a9db18ec56a6161029457d4d4f0083413feb98d3
soulayman <soulaymanjaa09@gmail.com>
2025-12-09 09:23:42 +0100

    TP-10 #DONE  created API routes

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const Authenticated =require('../middlewares/Authenticated');
- //testing gtihub
+
 
 router.post('/login', authController.login(req,res));
 router.post('/register', authController.register(req,res));
