26d4a6aa45b77fba2e37e4682458759b01a93fc0
soulayman <soulaymanjaa09@gmail.com>
2025-12-12 11:07:45 +0100

    create the update method

--- back-end/controllers/MaintenanceController.js
+++ back-end/controllers/MaintenanceController.js
@@ -1,36 +1,44 @@
-const maintenanceServie = require('../services/maintenanceServie')
+const maintenanceServie = require("../services/maintenanceServie");
 
+async function getAll(req, res) {
+  try {
+    const rules = maintenanceServie.getAll();
 
-async function getAll(req,res){
-try {
-    
- const rules = maintenanceServie.getAll();
-
- return res.status(200).json({rules});
-
-} catch (error) {
- console.log(error);
- return res.status(400).json({error:error.message})   
-}
+    return res.status(200).json({ rules });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
 }
 
-async function create(res,req){
-try {
-    
-    const rule = await maintenanceServie.create(req.body)
+async function create(res, req) {
+  try {
+    const rule = await maintenanceServie.create(req.body);
 
     res.status(201).json({
-        message:'rule created succesfully',
-        rule
-    }) 
-
-} catch (error) {
+      message: "rule created succesfully",
+      rule,
+    });
+  } catch (error) {
     console.log(error);
-    res.status(400).json({message:error.message})
+    res.status(400).json({ message: error.message });
+  }
 }
 
+async function update(req, res) {
+  try {
+    const id = req.params.id;
+    const rule = await maintenanceServie.update(id, req.body);
+
+    return res.status(201).json({
+      rule,
+      message: "Maintenance rule updated succesfully",
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 }
 
-
-
-module.exports = {getAll}
+module.exports = { getAll,update,create };
--- back-end/services/maintenanceServie.js
+++ back-end/services/maintenanceServie.js
@@ -1,40 +1,59 @@
 const MaintenanceRules = require("../models/MaintenanceRules");
 
-
-
-async function getAll(){
-try {
-
+async function getAll() {
+  try {
     const rules = await MaintenanceRules.find();
-    if(!rules||rules.length<0) throw new Error ('there are no rules ');
+    if (!rules || rules.length < 0) throw new Error("there are no rules ");
     return rules;
-
-} catch (error) {
-    throw new Error (error.message);
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
+
+async function create({ type, kms, days, description }) {
+  try {
+    let rule = new MaintenanceRules();
+
+    if (type) rule.type = type;
+    if (kms) rule.kms = kms;
+    if (days) rule.days = days;
+    if (description) rule.description = description;
+
+    await rule.save();
+
+    return rule;
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Error(`Rule with this type already exists`);
+    }
+    throw error;
+  }
 }
 
-async function create({type,kms,days,description}){
-    try {
-         
-    let rule=new MaintenanceRules();
 
-    if(type) rule.type=type
-    if(kms) rule.kms=kms
-    if(days) rule.days=days
-    if(description) rule.description=description;
-  
+
+async function update(id,{ type, kms, days, description }) {
+  try {
+
+    let rule = await MaintenanceRules.findById(id);
+    if(!rule) throw new Error("rule don't exist"); 
+
+     if (type) rule.type = type;
+    if (kms) rule.kms = kms;
+    if (days) rule.days = days;
+    if (description) rule.description = description;
+
     await rule.save();
 
     return rule
-        
-    } catch (error) {
-  if (error.code === 11000) {
-    throw new Error(`Rule with this type already exists`);
-  }
-  throw error;    }
 
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Error(`Rule with this type already exists`);
+    }
+    throw error;
+  }
 }
 
 
-module.exports = {getAll}
+module.exports = { getAll, update, create };
